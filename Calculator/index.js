35c95a47fafaf087865829e91fc134687a226d7c
Rafael Blanga <A01781442@tec.mx>
2023-08-30 22:55:59 -0600

    calculator codes added

---
+++ Calculator/index.js
@@ -0,0 +1,61 @@
+//set the values of the buttons in values
+
+var button1 = document.getElementsByClassName("char c1")
+button1 = 1;
+
+var button2 = document.getElementsByClassName("char c2")
+button2 = 2;
+
+var button3 = document.getElementsByClassName("char c3")
+button3 = 3;
+
+var button4 = document.getElementsByClassName("char c4")
+button4 = 4;
+
+var button5 = document.getElementsByClassName("char c5")
+button5 = 5;
+
+var button6 = document.getElementsByClassName("char c6")
+button6 = 6;
+
+var button7 = document.getElementsByClassName("char c7")
+button7 = 7;
+
+var button8 = document.getElementsByClassName("char c8")
+button8 = 8;
+
+var button9 = document.getElementsByClassName("char c9")
+button9 = 9;
+
+var button0 = document.getElementsByClassName("char c0")
+button0 = 0;
+
+var buttonPlus = document.getElementsByClassName("char mas op")
+buttonPlus = "+";
+
+var buttonMinus = document.getElementsByClassName("char menos op")
+buttonMinus = "-";
+
+var buttonTimes = document.getElementsByClassName("char multi op")
+buttonTimes = "*";
+
+var buttonDivide = document.getElementsByClassName("char divi op")
+buttonDivide = "/";
+
+var buttonEquals = document.getElementsByClassName("char equ op")
+buttonEquals = "=";
+
+var buttonClear = document.getElementsByClassName("char cancel op")
+buttonClear = "c";
+
+var buttonDot = document.getElementsByClassName("char cpunto")
+buttonDot = ".";
+
+
+//do a function so when i click a button it adds on the result
+
+function addOnResult() {
+    //add the pressed button to the resul
+    
+}
+
